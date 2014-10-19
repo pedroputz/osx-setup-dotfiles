13ea38c0a24df1929ca993cae9fb7684382f4b49
Pedro Putz <pedroputz@gmail.com>
2014-10-19 12:50:05 -0200

    adding colors back to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,33 @@
-echo ""
-echo "Welcome to the installation process :)"
+NOCOLOR=`tput sgr0`
+GREEN=`tput setaf 2`
+LGREEN=`tput bold; tput setaf 2`
+YELLOW=`tput bold; tput setaf 3`
+BLUE=`tput bold; tput setaf 4`
+MAGENTA=`tput bold; tput setaf 5`
 
 echo ""
-echo "The installer will use this dir: "
+echo "${GREEN}Welcome to the installation process :)${NOCOLOR}"
+
+echo ""
+echo "${YELLOW}The installer will use this dir: "
 cd ~
 pwd
-echo ""
+echo "${NOCOLOR}"
 
 echo "Homebrew is the first!"
 if [[ ! "$(type -P brew)" ]]; then
-    echo "- Installing..."
+    echo "- ${GREEN}Installing...${NOCOLOR}"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-    echo "- Already installed!"
+    echo "- ${BLUE}Already installed!${NOCOLOR}"
 fi
 
 if [[ "$(type -P brew)" ]]; then
-    echo "- Updating..."
+    echo "- ${GREEN}Updating...${NOCOLOR}"
     brew update
-    echo "- Analyzing..."
+    echo "- ${GREEN}Analyzing...${NOCOLOR}"
     brew doctor
-    echo "- Installing Formulas..."
+    echo "- ${GREEN}Installing Formulas...${NOCOLOR}"
     brew install git node
 fi
 echo ""
