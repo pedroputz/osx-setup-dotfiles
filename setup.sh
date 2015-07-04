0fd6b62250fb56fe7b6be14debd11ba6c66d03e2
Pedro Putz <pedroputz@gmail.com>
2015-07-04 18:36:55 -0300

    Updating setup.sh file with new functions

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+DOTFILESDIR="~/.dotfiles"
+SUBLIMETEXTDIR="~/Library/Application\ Support/Sublime\ Text\ 3"
+
 NOCOLOR=`tput sgr0`
 GREEN=`tput setaf 2`
 LGREEN=`tput bold; tput setaf 2`
@@ -14,7 +17,8 @@ cd ~
 pwd
 echo "${NOCOLOR}"
 
-echo "Homebrew"
+echo "${MAGENTA}Homebrew${NOCOLOR}"
+echo ""
 if [[ ! "$(type -P brew)" ]]; then
     echo "- ${GREEN}Installing...${NOCOLOR}"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -23,16 +27,41 @@ else
 fi
 
 if [[ "$(type -P brew)" ]]; then
+    echo ""
     echo "- ${GREEN}Updating...${NOCOLOR}"
     brew update
+    echo ""
     echo "- ${GREEN}Analyzing...${NOCOLOR}"
     brew doctor
+    echo ""
     echo "- ${GREEN}Installing Formulas...${NOCOLOR}"
     brew install git node
 fi
 echo ""
 
-echo "Sublime Text Configuration"
-echo "- Cloning pedroputz/sublimetext-confs.git..."
-git clone git@github.com:pedroputz/sublimetext-confs.git ~/Library/Application\ Support/Sublime\ Text\ 3
+echo "${MAGENTA}Dotfiles${NOCOLOR}"
+echo ""
+echo "- ${GREEN}Cloning pedroputz/osx-setup-dotfiles.git...${NOCOLOR}"
+if [ ! -d "$DOTFILESDIR" ]; then
+    git clone git@github.com:pedroputz/osx-setup-dotfiles.git "$DOTFILESDIR"
+else
+    cd "$DOTFILESDIR" && git pull && cd ~
+fi
+
+echo ""
+echo "- ${GREEN}Making links${NOCOLOR}"
+rm -f ~/.gitconfig ~/.bash_profile ~/.bash_aliases
+ln -s .dotfiles/.gitconfig ~/.gitconfig
+ln -s .dotfiles/.bash_profile ~/.bash_profile
+ln -s .dotfiles/.bash_aliases ~/.bash_aliases
+
+echo ""
+echo "${MAGENTA}Sublime Text Configuration${NOCOLOR}"
+echo ""
+echo "- ${GREEN}Cloning pedroputz/sublimetext-confs.git...${NOCOLOR}"
+if [ ! -d "$SUBLIMETEXTDIR" ]; then
+    git clone git@github.com:pedroputz/sublimetext-confs.git "$SUBLIMETEXTDIR"
+else
+    cd "$SUBLIMETEXTDIR" && git pull && cd ~
+fi
 echo ""
