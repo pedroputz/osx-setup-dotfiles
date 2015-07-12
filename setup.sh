4f7b4be829a12d21a157ecf8cbfc04ecd7c5c15a
Pedro Putz <pedroputz@gmail.com>
2015-07-11 21:59:46 -0300

    Fixing sublime path in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 DOTFILESDIR=$HOME"/.dotfiles"
-SUBLIMETEXTDIR=$HOME"/Library/Application\ Support/Sublime\ Text\ 3"
+SUBLIMETEXTDIR=$HOME"/Library/Application Support/Sublime Text 3"
 
 NOCOLOR=`tput sgr0`
 GREEN=`tput setaf 2`
